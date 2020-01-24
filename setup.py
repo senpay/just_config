6c5dcbcfd7cb617883fef504a4eed4dd036e9ba1
Alexander Pushkarev <alexspush@gmail.com>
2020-01-24 23:07:35 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+from just_config import version
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='just_config',
+    version=version.VERSION,
+    author='Alexander Pushkarev',
+    author_email='alexspush@gmail.com',
+    description='Minimalistic library to resolve configuration for a python program',
+    license='MIT',
+    url='http://packages.python.org/just_config',
+    packages=['just_config'],
+    long_description=read('README.MD')
+)
